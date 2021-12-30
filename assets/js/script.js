c70188aad1b9fa0a62926ac565f70e2ab2717c1b
Kate Johnson <johnson.kat.g@gmail.com>
2021-12-30 15:34:31 -0500

    initial html and css setup

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var quizQuestions = document.getElementById("#quiz");
+var quizResults = document.getElementById("#results");
+var previousButton = document.getElementById("#previous");
+var nextButton = document.getElementById("#next");
+var submitButton = document.getElementById("#submit");
